29d304a579535a357f3260ec92dfa778d932e079
t912772863 <912772863@qq.com>
2018-08-02 11:25:55 +0800

    添加mybatis自动动成dao, entity, mapper的插件 . 并生成用户表相关基本方法

---
+++ src/main/java/com/tian/dao/mapper/UserInfoMapper.java
@@ -0,0 +1,17 @@
+package com.tian.dao.mapper;
+
+import com.tian.dao.entity.UserInfo;
+
+public interface UserInfoMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UserInfo record);
+
+    int insertSelective(UserInfo record);
+
+    UserInfo selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(UserInfo record);
+
+    int updateByPrimaryKey(UserInfo record);
+}
